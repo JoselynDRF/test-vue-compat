b91bce9ab3fe122f97af313669cb68def9a7454c
Joselyn Ferreira <joselyn.ferreira@infraspeak.com>
2024-02-29 12:15:16 +0000

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="message" />
+    <button @click="onClick">Click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+type Data = {
+  message: string
+};
+
+export default Vue.extend({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld
+  },
+
+  data (): Data {
+    return {
+      message: 'Test @vue/compat'
+    }
+  },
+
+  methods: {
+    onClick (): void {
+      console.log('clicked')
+    }
+  }
+})
+</script>
